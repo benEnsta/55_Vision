c0bd81a88f60196b5e8fb394cf9e4a0296350d1b
Benoit DESROCHERS <ben.ensta@gmail.com>
2014-02-15 17:31:33 +0100

    Detection fonctionnelle !!!!!!!

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ static void help(void)
                 );
 }
 // various tracking parameters (in seconds)
-const double MHI_DURATION = 0.2;
+const double MHI_DURATION = 0.5;
 const double MAX_TIME_DELTA = 0.5;
 const double MIN_TIME_DELTA = 0.05;
 // number of cyclic frame buffer used for motion detection
@@ -63,73 +63,62 @@ static void  update_mhi( Mat &img, Mat &dst, int diff_threshold )
     if( init == false ) {
 
         cout << "here init" << endl;
-        buf =  vector<Mat>(N,Mat::zeros(img.rows,img.cols,CV_8U));
-        mhi.zeros(img.rows,img.cols,CV_8U);
+        buf =  vector<Mat>(N);
+        for(uint i = 0; i < N; i++){
+            buf[i].create(img.rows, img.cols,CV_8UC1);
+        }
+        mhi  = Mat::zeros(img.rows,img.cols,CV_32FC1);
         init = true;
-//        orient.zeros()
-
-//        mhi = cvCreateImage( size, IPL_DEPTH_32F, 1 );
-//        cvZero( mhi ); // clear MHI at the beginning
-//        orient = cvCreateImage( size, IPL_DEPTH_32F, 1 );
-//        segmask = cvCreateImage( size, IPL_DEPTH_32F, 1 );
-//        mask = cvCreateImage( size, IPL_DEPTH_8U, 1 );
     }
 
     // put image inside the framebuffer
-
-    cvtColor( img, buf[last], CV_BGR2GRAY ); // convert frame to grayscale
-
+    Mat buf_tmp;
+    cvtColor( img, buf_tmp, CV_BGR2GRAY ); // convert frame to grayscale
+    buf_tmp.copyTo(buf[last]);
     cout << "write at "<< &buf[last] << endl;
-//    cout << "here cvtColor Done" << endl;
+
     // update indices
     idx2 = (last + 1) % N; // index of (last - (N-1))th frame
     last = idx2;
 
 
-//    silh = buf[idx2];
-
-
-    Mat m1 = buf.at(idx1);
-    Mat m2 = buf.at(idx2);
-    cout << "test de comparaison " << idx1 << " " << idx2 << " " << &m1.data << " " << &m2.data << endl;
-    cout << &buf.at(0) << " " <<  &buf.at(1) << " " <<  &buf.at(2)<< " " <<  &buf.at(3) << endl;
-    for(uint i = 0; i < m1.rows ; i++){
-        for(uint j = 0; j < m1.cols ; j++){
-            if(m1.at<uchar>(i,j) - m2.at<uchar>(i,j) != 0 ){
-                cout << "not the same matrix" << endl;
-                break;
-            }
-        }
-    }
-//    cout << "same matrix !!!" << endl;
-
+    silh.zeros(img.rows, img.cols,CV_8UC1);
     absdiff(buf.at(idx1), buf.at(idx2), silh ); // get difference between frames
-//    cout << "here absdiff Done  " << idx1 << " " << idx2 << " "  << last << endl;
-    dst = silh;
+
     threshold( silh, silh, diff_threshold, 1, CV_THRESH_BINARY ); // and threshold it
-//    cout << "here threshold Done" << endl;
+    cout << "here threshold Done" << endl;
 //    dst = silh;
-//    updateMotionHistory( silh, mhi, timestamp, MHI_DURATION ); // update MHI
-//    cout << "here updateMotionHistory Done" << endl;
+    cout << silh.type() << " " << CV_8UC1 << " " << mhi.type() << " " << CV_32FC1 << endl;
+    updateMotionHistory( silh, mhi, timestamp, MHI_DURATION ); // update MHI
+    cout << "here updateMotionHistory Done" << endl;
 
     // convert MHI to blue 8u image
 //    cvCvtScale( mhi, mask, 255./MHI_DURATION,
 //                (MHI_DURATION - timestamp)*255./MHI_DURATION );
-//    mhi.convertTo(mask,CV_8U, 255./MHI_DURATION,
-//                (MHI_DURATION - timestamp)*255./MHI_DURATION );
+    mhi.convertTo(mask,CV_8U, 255./MHI_DURATION,
+                (MHI_DURATION - timestamp)*255./MHI_DURATION );
 //    dst.zeros()
-//    vector<Mat> merge_vect(4,Mat::zeros(mask.rows, mask.cols,CV_8U));
-//    merge_vect[0] = mask;
-//    cv::merge( merge_vect, dst );
+    vector<Mat> merge_vect(4,Mat::zeros(mask.rows, mask.cols,CV_8U));
+    merge_vect[0] = mask;
+    cv::merge( merge_vect, dst );
 
     // calculate motion gradient orientation and valid orientation mask
-//    calcMotionGradient( mhi, mask, orient, MAX_TIME_DELTA, MIN_TIME_DELTA, 3 );
+    calcMotionGradient( mhi, mask, orient, MAX_TIME_DELTA, MIN_TIME_DELTA, 3 );
 
 
     // segment motion: get sequence of motion components
     // segmask is marked motion components map. It is not used further
-//    segmentMotion( mhi, segmask, storage, timestamp, MAX_TIME_DELTA );
+    segmentMotion( mhi, segmask, storage, timestamp, MAX_TIME_DELTA );
 
+    for( i = 0; i < storage.size(); i++ ){
+        Rect comp_rect = storage.at(i);
+
+        if( comp_rect.width + comp_rect.height < 100 ) // reject very small components
+            continue;
+        color = CV_RGB(255,0,0);
+        magnitude = 30;
+        rectangle(img,storage[i],color);
+    }
     // iterate through the motion components,
     // One more iteration (i == -1) corresponds to the whole image (global motion)
 //    for( i = -1; i < seq->total; i++ ) {
